b184bfd02f45f146810b859c2dc77cf7fc89589d
Cal Friedman School <cffriedm@ucsc.edu>
2025-07-13 20:06:34 -0700

    direction arrow basic working

--- Assets/Scripts/DirectionArrow.cs
+++ Assets/Scripts/DirectionArrow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class directionArrow : MonoBehaviour
+public class DirectionArrow : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
@@ -17,11 +17,11 @@ public class directionArrow : MonoBehaviour
     void Update()
     {
         Vector3 directionVector = new Vector3(0, 0, 0);
-        Vector3 targetVector = target.position - transform.position;
+        Vector3 targetVector = (target.position - transform.position).normalized;
         float stepValue = 1.0f * Time.deltaTime;
 
-        directionVector = Vector3.RotateTowards(transform.forward, targetVector, 100, 0.0f);
+        //directionVector = Vector3.RotateTowards(transform.forward, targetVector, 100, 0.0f);
         print(transform.up + ", " + directionVector + ", " + targetVector);
-        transform.LookAt(directionVector, Vector3.forward * -1); //rotation = Quaternion.LookRotation(directionVector); //
+        transform.right = targetVector;//, Vector3.forward * -1); //rotation = Quaternion.LookRotation(directionVector); //
     }
 }
