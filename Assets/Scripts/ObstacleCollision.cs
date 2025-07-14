59bccc55f3ba90127602f98b09596fe01a088b79
clayxaviermusic <157855925+clayxaviermusic@users.noreply.github.com>
2025-07-13 20:43:33 -0700

    Collision SFX

--- Assets/Scripts/ObstacleCollision.cs
+++ Assets/Scripts/ObstacleCollision.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using FMODUnity;
+using FMOD.Studio;
 public class ObstacleCollision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
@@ -11,14 +12,19 @@ public class ObstacleCollision : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Player"))
         {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerDamage");
             collision.gameObject.GetComponent<PlayController>().RepairEntryState();
             obstacleHealth.OnDamage(5);
+            
         }
         else if (collision.gameObject.CompareTag("Cannonball"))
         {
+            
             Debug.Log("Hit this obstacle " + gameObject.name);
             obstacleHealth.OnDamage(1);
             Destroy(collision.gameObject);
+            FMODUnity.RuntimeManager.PlayOneShot("event:/EnemyDamage");
+            
         }
     }
 }
